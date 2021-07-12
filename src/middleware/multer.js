a09a91fb06e808bd1f0af52d90f9b9cf71581355
Efendi <fndsanto@gmail.com>
2021-07-13 00:25:31 +0700

    fix bug for upload avatar

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -3,18 +3,32 @@
 const multer = require('multer');
 const fs = require('fs');
 
-exports.upload = multer({
-    storage : multer.diskStorage({
-        destination : function (req, file, cb) {
-            let destination = req.url.split('/');
-            let customPath = './uploads/' + destination[2] + "/";
-            fs.mkdirSync(customPath, { recursive : true });
-            cb(null, customPath);
-        },
-        filename : function (req, file, cb) {
-            let filename = req.user.id + "-" + file.fieldname + "-" + file.originalname.toLowerCase();
-            req.body.filename = filename;
-            cb(null, filename);
-        }
-    })    
-});
+// exports.upload = multer({
+//     storage : multer.diskStorage({
+//         destination : function (req, file, cb) {
+//             let destination = req.url.split('/');
+//             let customPath = './uploads/' + destination[2] + "/";
+//             fs.mkdirSync(customPath, { recursive : true });
+//             cb(null, customPath);
+//         },
+//         filename : function (req, file, cb) {
+//             let filename = req.user.id + "-" + file.fieldname + "-" + file.originalname.toLowerCase();
+//             req.body.filename = filename;
+//             cb(null, filename);
+//         }
+//     })    
+// });
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        let destination = req.url.split('/');
+        let customPath = './uploads/' + destination[2] + "/";
+        cb(null, customPath)
+    },
+    filename: function (req, file, cb) {
+        let filename = req.user.id + "-" + file.fieldname + "-" + file.originalname.toLowerCase();
+        req.body.filename = filename;
+        cb(null, filename)
+    }
+})
+exports.upload = multer({ storage: storage })
